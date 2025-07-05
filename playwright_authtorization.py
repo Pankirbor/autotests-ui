68f200fb09f6accdcda96b35e2c590276568d95f
Кирилл Панов <lliril_05@list.ru>
2025-07-05 22:34:58 +0300

    Введение в Playwright. Практика с Playwright

--- playwright_authtorization.py
+++ playwright_authtorization.py
@@ -1,14 +1,12 @@
 from playwright.sync_api import sync_playwright, expect
 
-with sync_playwright() as p:
-    print("Starting browser...")
-    browser = p.chromium.launch(headless=False)
-    print("Browser started")
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
     page = browser.new_page()
     page.goto(
         "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login"
     )
-    print("Page opened")
+
     email_input = page.locator(
         '//div[@data-testid="login-form-email-input"]//div//input'
     )
@@ -18,21 +16,14 @@ with sync_playwright() as p:
     )
     password_input.fill("password")
 
-    # Находим кнопку "Login" и кликаем на нее
     login_button = page.locator('//button[@data-testid="login-page-login-button"]')
     login_button.click()
 
-    # Проверяем, что появилось сообщение об ошибке
     wrong_email_or_password_alert = page.locator(
         '//div[@data-testid="login-page-wrong-email-or-password-alert"]'
     )
-    expect(
-        wrong_email_or_password_alert
-    ).to_be_visible()  # Проверяем видимость элемента
-    expect(wrong_email_or_password_alert).to_have_text(
-        "Wrong email or password"
-    )  # Проверяем текст
+    expect(wrong_email_or_password_alert).to_be_visible()
 
-    # Пауза на 5 секунд, чтобы увидеть результат
+    expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
 
     page.wait_for_timeout(5000)
