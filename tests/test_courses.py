8824ae4fbc4ca88a2d922ce1d8b9f70d6780c12e
Кирилл Панов <lliril_05@list.ru>
2025-07-07 15:16:04 +0300

    Маркировки Pytest. Практика работы с pytest маркировкам

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,6 +1,9 @@
+import pytest
 from playwright.sync_api import sync_playwright, expect
 
 
+@pytest.mark.courses
+@pytest.mark.regression
 def test_empty_courses_list():
     """
     Тестирует отображение страницы курсов при пустом списке курсов.
