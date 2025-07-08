2ddb03247e54a47e262c59b64795bc26b2d5748e
Кирилл Панов <lliril_05@list.ru>
2025-07-08 12:27:38 +0300

    Параметризация Pytest. Практика работы с pytest параметризацией

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,32 +1,60 @@
 import pytest
 
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import expect, Page
 
 
 @pytest.mark.regression
 @pytest.mark.authorization
-def test_wrong_email_or_password_authorization():
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        page = browser.new_page()
+@pytest.mark.parametrize(
+    "email, password",
+    [
+        ("  ", "password"),
+        ("user.name@gmail.com", "  "),
+        ("user.name@gmail.com", "password"),
+    ],
+)
+def test_wrong_email_or_password_authorization(
+    chromium_page: Page, email: str, password: str
+):
+    """
+    Тестирует авторизацию с неправильным email или паролем.
 
-        page.goto(
-            "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login"
-        )
+    Цель теста: проверить, что система корректно обрабатывает попытку входа с пустыми
+    или некорректными данными и отображает соответствующее сообщение об ошибке.
 
-        email_input = page.get_by_test_id("login-form-email-input").locator("input")
-        email_input.fill("user.name@gmail.com")
+    Параметры:
+    - `email` (str): email для ввода в поле email.
+    - `password` (str): пароль для ввода в поле пароля.
 
-        password_input = page.get_by_test_id("login-form-password-input").locator(
-            "input"
-        )
-        password_input.fill("password")
+    Шаги:
+    1. Переходит на страницу авторизации.
+    2. Вводит указанный email и пароль.
+    3. Нажимает кнопку "Войти".
+    4. Проверяет, что появляется уведомление о неверном email или пароле.
+    5. Проверяет текст уведомления на соответствие ожидаемому значению.
 
-        login_button = page.get_by_test_id("login-page-login-button")
-        login_button.click()
+    Использует: параметризацию pytest, методы playwright для работы с DOM и проверки условий.
+    """
 
-        wrong_email_or_password_alert = page.get_by_test_id(
-            "login-page-wrong-email-or-password-alert"
-        )
-        expect(wrong_email_or_password_alert).to_be_visible()
-        expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
+    chromium_page.goto(
+        "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login"
+    )
+
+    email_input = chromium_page.get_by_test_id("login-form-email-input").locator(
+        "input"
+    )
+    email_input.fill(email)
+
+    password_input = chromium_page.get_by_test_id("login-form-password-input").locator(
+        "input"
+    )
+    password_input.fill(password)
+
+    login_button = chromium_page.get_by_test_id("login-page-login-button")
+    login_button.click()
+
+    wrong_email_or_password_alert = chromium_page.get_by_test_id(
+        "login-page-wrong-email-or-password-alert"
+    )
+    expect(wrong_email_or_password_alert).to_be_visible()
+    expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
