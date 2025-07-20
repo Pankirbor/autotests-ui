ce3445857c2ff4ac0cae5d4d1c1f0177d3acddad
Кирилл Панов <lliril_05@list.ru>
2025-07-20 22:31:55 +0300

    Добавлен отчет о покрытии приложения тестами в конвеер CI

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import Any, Self
 
-from pydantic import BaseModel, EmailStr, DirectoryPath, HttpUrl, FilePath
+from pydantic import BaseModel, EmailStr, DirectoryPath, Field, HttpUrl, FilePath
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
