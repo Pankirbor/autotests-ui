ef6cedae377545352ba59344950f5fe36c36d52d
Кирилл Панов <lliril_05@list.ru>
2025-07-21 00:18:32 +0300

    Практикуемся в интеграции покрытия в UI тесты. Практическое задание: Работа с инструментом ui-coverage-tool

--- elements/button.py
+++ elements/button.py
@@ -1,3 +1,4 @@
+from typing import Self
 import allure
 
 from playwright.sync_api import expect
@@ -22,7 +23,7 @@ class Button(BaseElement):
         check_disabled: Проверяет, что кнопка недоступна для взаимодействия.
     """
 
-    def check_enabled(self, nth: int = 0, **kwargs):
+    def check_enabled(self, nth: int = 0, **kwargs) -> Self:
         """
         Проверяет, что кнопка включена (доступна для нажатия).
 
@@ -37,8 +38,9 @@ class Button(BaseElement):
             expect(locator).to_be_enabled()
 
         self.track_coverage(ActionType.ENABLED, nth, **kwargs)
+        return self
 
-    def check_disabled(self, nth: int = 0, **kwargs):
+    def check_disabled(self, nth: int = 0, **kwargs) -> Self:
         """
         Проверяет, что кнопка выключена (недоступна для нажатия).
 
@@ -53,3 +55,4 @@ class Button(BaseElement):
             expect(locator).to_be_disabled()
 
         self.track_coverage(ActionType.DISABLED, nth, **kwargs)
+        return self
